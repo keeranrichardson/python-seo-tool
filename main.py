5729866fa39d6274ca0827007a864d95f54ed645
keeran richardson <keeranrichardson@gmail.com>
2021-09-26 14:25:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from htmlreporter import HTMLReporter
 scanner = Scanner('https://keeranrichardson.com')
 scanner.scan()
 
-#print(HTMLReporter(scanner.getResults()).makeReport())
-
 file = open("htmlReport.html", "w")
 file.write(HTMLReporter(scanner.getResults()).makeReport())
 file.close()
