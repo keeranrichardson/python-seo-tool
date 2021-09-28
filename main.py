57c296c0dae49eb3a037d60b707e308c5f44e56c
keeran richardson <keeranrichardson@gmail.com>
2021-09-28 19:01:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from scanner import Scanner
 from htmlreporter import HTMLReporter
 from browserController import BrowserController
+import datetime
+import re
 
 scanner = Scanner('https://keeranrichardson.com')
 scanner.scan()
 
-fileName = str(input("enter the name of html report file"))+".html"
+now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+fileName = str(input("enter the name of html report file, default file name = "+now))
+
+fileName = re.sub('[^A-Za-z0-9_\-\.]+', '', fileName)+'.html'
+
+if fileName == '.html':
+    fileName = now+'.html'
+
 file = open(fileName, "w")
 file.write(HTMLReporter(scanner.getResults()).makeReport())
 file.close()
