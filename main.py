069a5d89d2379ec5f23279b59e227b8c3ffd6e03
keeran richardson <keeranrichardson@gmail.com>
2021-09-29 17:22:23 +0100

    added fileName class

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from scanner import Scanner
 from htmlreporter import HTMLReporter
 from browserController import BrowserController
-import datetime
-import re
+from fileName import FileName
 
 scanner = Scanner('https://keeranrichardson.com')
 scanner.scan()
 
-now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-
-fileName = str(input("enter the name of html report file, default file name = "+now))
-
-fileName = re.sub('[^A-Za-z0-9_\-\.]+', '', fileName)+'.html'
-
-if fileName == '.html':
-    fileName = now+'.html'
+fileName = FileName().getHTMLReportFileName()
 
 file = open(fileName, "w")
 file.write(HTMLReporter(scanner.getResults()).makeReport())
