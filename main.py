cff921736e0a61f3d7a2b90713a76b6fa369e4e7
keeran richardson <keeranrichardson@gmail.com>
2021-10-03 13:04:09 +0100

    reports in subfolder
    reports now save into subfolder

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from scanner import Scanner
 from htmlreporter import HTMLReporter
 from browserController import BrowserController
 from fileName import FileName
+import os
 
 scanner = Scanner('https://keeranrichardson.com')
 scanner.scan()
 
 fileName = FileName().getHTMLReportFileName()
 
-file = open(fileName, "w")
+directory = "reports"
+parentDir = os.getcwd() #gets current working director
+path = os.path.join(parentDir, directory)
+try:
+    os.mkdir(path)
+except OSError as error:
+    pass
+
+pathAndFileName = os.path.join(path, fileName)
+
+file = open(pathAndFileName, "w")
 file.write(HTMLReporter(scanner.getResults()).makeReport())
 file.close()
 
-BrowserController().open(fileName)
+BrowserController().open(pathAndFileName)
 
