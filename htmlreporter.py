acf380b30a14ef9ba8cf33df2bd0f34163f8fab4
keeran richardson <keeranrichardson@gmail.com>
2021-10-21 19:13:44 +0100

    Format strings instead of concatenation on report

--- htmlreporter.py
+++ htmlreporter.py
@@ -27,7 +27,7 @@ class HTMLReporter:
                     </body>
                 </html>'''
     
-        scannerStartDateLine = "<h2>Date and time of scan: "+self.scannerResult.getStartDateTime()+"</h2>\n"
+        scannerStartDateLine = "<h2>Date and time of scan: {}</h2>\n".format(self.scannerResult.getStartDateTime())
 
 #https://stackoverflow.com/questions/538666/format-timedelta-to-string
 
@@ -36,18 +36,18 @@ class HTMLReporter:
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
 #https://docs.python.org/3/library/string.html#format-string-syntax
-        duration = '{:02} hours, {:02} minutes, {:02} seconds'.format(int(hours), int(minutes), int(seconds))
-        timeDifferenceLine = "<p>Duration of scan: "+duration+"</p>"
+        duration = '{:02} hours, {:02} minutes, {:02} seconds'.format(hours, minutes, seconds)
+        timeDifferenceLine = "<p>Duration of scan: {}</p>".format(duration)
 
         numberOfUrlsFound = str(len(results))
-        numberOfUrlsFoundLine = "<p>Number of URLs found = "+numberOfUrlsFound+"</p>"
+        numberOfUrlsFoundLine = "<p>Number of URLs found = {}</p>".format(numberOfUrlsFound)
 
         middle = ""
 
         lineTemplate = "<li>{} {} [{}]</li>"
 
         for result in results:
-            statusCodeLink = self.aTag("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/"+str(result.getStatusCode()),str(result.getStatusCode()))
+            statusCodeLink = self.aTag("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{}".format(result.getStatusCode()),str(result.getStatusCode()))
             resultUrlLink = self.aTag(result.getURL(), result.getURL())
             parentUrl = self.aTag(result.getParentUrl(), "parent page")
 
