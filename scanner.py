5e6191a6ec17fdce1e7226887c8ff3cc8151bd1e
keeran richardson <keeranrichardson@gmail.com>
2021-11-07 16:48:06 +0000

    Update scanner.py
    Treat URL with trailing slash as the same as without and fixed duplicates

--- scanner.py
+++ scanner.py
@@ -13,6 +13,8 @@ class Scanner:
         self.urlsToScan = [self.startingUrl]
         self.urlsScanned = []
         self.urlsStatusChecked = []
+        #config
+        self.treatUrlsWithEndingSlashSameAsWithout = True 
     
     def scan(self):
         while len(self.urlsToScan) > 0:
@@ -26,6 +28,8 @@ class Scanner:
         if webPage.isUrlScannable():
             self.results.addUrlScanned(url)
             for aLink in webPage.findLinks():
+                if aLink.endswith('/'):
+                    print("slash found")
                 if aLink in self.urlsStatusChecked:
                     print("status checked already")
                 else:
@@ -33,20 +37,34 @@ class Scanner:
                     if link.isScannable():
                         self.addUrlToScan(aLink)
                     print(link.getStatus(), aLink)
-                    self.results.add(UrlResult(aLink, link.getStatus(), url))
-                    self.urlsStatusChecked.append(aLink)
+                    if not self.haveScannedAlready(aLink):
+                        self.results.add(UrlResult(aLink, link.getStatus(), url))
+                        self.urlsStatusChecked.append(aLink)
 
     def getResults(self):
         return self.results
 
     def addUrlToScan(self, url):
-        parsedUrl = urlparse(url)
-        if url not in self.urlsScanned:
-            if self.restrictToDomain in parsedUrl.netloc:
-                self.urlsToScan.append(url)
+        if not self.haveScannedAlready(url) and self.isAllowedToBeScanned(url):
+            self.urlsToScan.append(url)
 
     def getUrlsToScan(self):
         return self.urlsToScan
+
+    def haveScannedAlready(self, url):
+        if self.treatUrlsWithEndingSlashSameAsWithout:
+            if url.endswith('/'):
+                url = url.removesuffix('/')
+        if url not in self.urlsScanned:
+            return False
+        return True
+
+    def isAllowedToBeScanned(self, url):
+        parsedUrl = urlparse(url)
+        if self.restrictToDomain in parsedUrl.netloc:
+                return True
+        return False
+
 
 class ScannerResults:
     def __init__(self):
